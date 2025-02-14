9d8f28054afe57dbcb1249c5d202bf6531342163
rvrutan <rutanroni@gmail.com>
2025-02-13 19:00:47 -0500

    middleware updated

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    port: 3000,
+    port: 3001,
     open: true,
     proxy: {
       '/api': {
--- server/src/middleware/auth.ts
+++ server/src/middleware/auth.ts
@@ -1,27 +1,23 @@
-import { Request, Response, NextFunction } from "express";
-import jwt from "jsonwebtoken";
+import { Request, Response, NextFunction } from 'express';
+import jwt from 'jsonwebtoken';
 
 interface JwtPayload {
   username: string;
 }
 
-export const authenticateToken = (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  // verify the token exists and add the user data to the request object
+export const authenticateToken = (req: Request, res: Response, next: NextFunction) => {
   const authHeader = req.headers.authorization;
 
-  if  (authHeader) {
+  if (authHeader) {
     const token = authHeader.split(' ')[1];
 
-    const secretKey = process.env.JWT_SCRET_KEY || '';
+    const secretKey = process.env.JWT_SECRET_KEY || '';
 
     jwt.verify(token, secretKey, (err, user) => {
       if (err) {
         return res.sendStatus(403);
       }
+
       req.user = user as JwtPayload;
       return next();
     });
